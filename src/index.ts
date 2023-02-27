87d32238f70c753d64d5fbb18939c37157dd9e18
Marco Rojas <10587527+MrcRjs@users.noreply.github.com>
2023-02-27 09:25:54 -0500

    Merge all source code into a single index file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 /* eslint-disable no-console */
-
-import {
-  blockAGHServices,
-  getAGHBlockedStatus,
-  getBlockableServicesIDs,
-  unblockALLAGHServices,
-} from "./AGHService/index.js";
+import fetch, { RequestInit } from "node-fetch";
+const { AGH_SERVER, AGH_USER, AGH_PASS } = process.env;
 
 const USAGE_MSG =
   "aghblock [block|unblock|unblockall|list|status] <service,service2,service3>";
 
-(async function init() {
+async function main() {
   const envVarsDefined = verifyDefinedEnvVariables([
     "AGH_SERVER",
     "AGH_USER",
@@ -58,7 +53,7 @@ const USAGE_MSG =
     console.error(err);
     return -1;
   }
-})();
+}
 
 function verifyDefinedEnvVariables(requiredVars: string[] = []) {
   const missingVars: string[] = [];
@@ -76,3 +71,141 @@ function verifyDefinedEnvVariables(requiredVars: string[] = []) {
 
   return { missing: false };
 }
+
+const AGH_ENDPOINTS = {
+  /**
+   * Get available services to use for blocking
+   * ```
+   *     'get':
+   *      'tags':
+   *      - 'blocked_services'
+   *      'operationId': 'blockedServicesAll'
+   *      'responses':
+   *        '200':
+   *          'description': 'OK.'
+   *          'content':
+   *            'application/json':
+   *              'schema':
+   *                '$ref': '#/components/schemas/BlockedServicesAll'
+   * ```
+   */
+  BLOCKABLE_SERVICES: "/blocked_services/all",
+  /**
+   * Get blocked services list
+   * ```
+   * 'get':
+   *      'tags':
+   *      - 'blocked_services'
+   *      'operationId': 'blockedServicesList'
+   *      'responses':
+   *        '200':
+   *          'description': 'OK.'
+   *          'content':
+   *            'application/json':
+   *              'schema':
+   *                '$ref': '#/components/schemas/BlockedServicesArray'
+   * ```
+   */
+  CURRENT_BLOCKED_SERVICES: "/blocked_services/list",
+  /**
+   * Set blocked services list
+   * ```
+   *   '/blocked_services/set':
+   *    'post':
+   *      'tags':
+   *      - 'blocked_services'
+   *      'operationId': 'blockedServicesSet'
+   *      'requestBody':
+   *        'content':
+   *          'application/json':
+   *            'schema':
+   *              '$ref': '#/components/schemas/BlockedServicesArray'
+   * ```
+   */
+  SET_BLOCKED_SERVICES: "/blocked_services/set",
+};
+
+interface BlockableService {
+  id: string;
+  name: string;
+  icon_svg: string;
+  rules: [];
+}
+
+async function fetchAGHAPI(
+  route = "/status",
+  options = { method: "GET" },
+  body?: object
+) {
+  const params: RequestInit = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Basic ${Buffer.from(`${AGH_USER}:${AGH_PASS}`).toString(
+        "base64"
+      )}`,
+    },
+    ...options,
+    body: body ? JSON.stringify(body) : undefined,
+  };
+  const response = await fetch(AGH_SERVER + route, params);
+
+  if (response.status >= 400) {
+    throw new Error(`${response.status} ${response.statusText}`);
+  }
+
+  if (params.method === "GET") {
+    return await response.json();
+  }
+
+  return response;
+}
+
+export async function blockAGHServices(services: string[]) {
+  // TODO: Validate services input is an actual blockable service
+  // const blockableServicesRes = await getBlockableServicesIDs();
+
+  const blockedServices = await fetchAGHAPI(
+    AGH_ENDPOINTS.CURRENT_BLOCKED_SERVICES
+  );
+
+  const uniqueServices: string[] = Array.from(
+    new Set([...services, ...blockedServices])
+  );
+
+  await fetchAGHAPI(
+    AGH_ENDPOINTS.SET_BLOCKED_SERVICES,
+    { method: "POST" },
+    uniqueServices
+  );
+
+  const blockedServicesUpdated = await fetchAGHAPI(
+    AGH_ENDPOINTS.CURRENT_BLOCKED_SERVICES
+  );
+
+  console.log("[AGH] Now blocking ", JSON.stringify(blockedServicesUpdated));
+}
+
+export async function unblockALLAGHServices() {
+  const blockServicesResponse = await fetchAGHAPI(
+    AGH_ENDPOINTS.SET_BLOCKED_SERVICES,
+    { method: "POST" },
+    []
+  );
+  console.log(blockServicesResponse);
+}
+
+export async function getBlockableServicesIDs() {
+  const blockableServicesRes = await fetchAGHAPI(
+    AGH_ENDPOINTS.BLOCKABLE_SERVICES
+  );
+  return blockableServicesRes.blocked_services.map(
+    (bserv: BlockableService) => bserv.id
+  );
+}
+
+export async function getAGHBlockedStatus() {
+  return await fetchAGHAPI(AGH_ENDPOINTS.CURRENT_BLOCKED_SERVICES);
+}
+
+main();
